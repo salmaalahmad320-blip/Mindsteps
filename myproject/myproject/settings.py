b122c73d4cae49d74e4466748ee2ac2943e8c7fb
salmaalahmad320-blip <salmaalahmad320@gmail.com>
2026-04-11 08:16:32 +0300

    Removed speech app and updated project

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,8 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'speech',      # تطبيق النطق
-    'assessment',  # تطبيق الاختبار
+    'assessment',
 ]
 
 MIDDLEWARE = [
